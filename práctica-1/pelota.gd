beee1b4194a2becf7a737cd1dcecae4dbd1548df
carluandraster <99754800+carluandraster@users.noreply.github.com>
2026-04-01 11:44:04 -0300

    Add files via upload

--- práctica-1/pelota.gd
+++ práctica-1/pelota.gd
@@ -1,14 +1,17 @@
 extends RigidBody2D
 
+class_name Pelota
+
 const FUERZA_DE_IMPULSO = -200.0
 const VELOCIDAD = 150
 
-# Called when the node enters the scene tree for the first time.
+
+	# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var toca_el_piso = get_colliding_bodies().size() > 0
 	if toca_el_piso and Input.is_action_pressed("ui_up"):
